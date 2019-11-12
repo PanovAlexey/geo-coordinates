8b5583054106e9974b58d2069cb6070327f06a33
Panov Alexey <alexeykapanov@gmail.com>
2019-11-12 14:25:00 +0300

    Create exceptions for GeoCoordinates package.

---
+++ src/Exceptions/GeoCoordinatesException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeblogPro\GeoCoordinates\Exception;
+
+/**
+ * This class represents a generic error.
+ */
+class GeoCoordinatesException extends \Exception
+{
+}
---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeblogPro\GeoCoordinates\Exception;
+
+class InvalidArgumentException extends GeoCoordinatesException
+{
+
+}
